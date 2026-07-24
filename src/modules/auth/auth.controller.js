bf5be774847b81aad0c1d3be196ac4fec3699fec
Junayed Ali Khan <jjunayedalikhan@gmail.com>
2026-07-24 22:58:58 +0530

    Implement Login Controller

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -36,6 +36,7 @@ exports.login = async (req, res) => {
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
     const isMatch = await user.comparePassword(password);
+
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
 
@@ -43,7 +44,15 @@ exports.login = async (req, res) => {
       expiresIn: "1d",
     });
 
-    res.json({ token, role: user.role });
+    res.json({
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
